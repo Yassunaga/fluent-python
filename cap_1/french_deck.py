e945a3ac634eb83946f9da9ba7782e65b625e082
C1313333 Gabriel Bueno Yassunaga <c1313333@interno.bb.com.br>
2021-04-12 23:58:17 -0300

    :truck: :sparkles: Adding features and updating files structure

--- cap_1/french_deck.py
+++ cap_1/french_deck.py
@@ -1,4 +1,5 @@
 import collections
+from random import choice
 
 Card = collections.namedtuple('Card', ['rank', 'suit'])
 
@@ -19,3 +20,21 @@ class FrenchDeck:
 
     def __len__(self):
         return len(self._cards)
+
+
+deck = FrenchDeck()
+print(choice(deck))
+for card in deck:
+    print(card)
+
+
+suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+
+
+def spades_high(card):
+    rank_value = FrenchDeck.ranks.index(card.rank)
+    return rank_value * len(suit_values) + suit_values[card.suit]
+
+
+for card in sorted(deck, key=spades_high):
+    print(card)
